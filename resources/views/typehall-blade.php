2509848add9e96b29d97557176c951355c4750a4
roseairynnn <2022937777@student.uitm.edu.my>
2023-11-29 15:22:02 +0800

    toggle update
    - update codes for toggle in footer.blade.php

---
+++ resources/views/typehall-blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Type | Wedhall</title>
+
+    <!--link url head-->
+    @include('components.head')
+
+</head>
+<body>
+    <!--header-->
+    <div class="header-main">
+        @include('components.header')
+    </div>
+
+    <!--sidebar nav-->
+    <div class="sidebar-main">
+        @include('components.nav')
+    </div>
+
+    
+</body>
+</html>
